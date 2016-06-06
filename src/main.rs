a045bed4ed61ac01e451e19b4c75423012d0de94
Daniel Toebe <dtoebe@gmail.com>
2016-06-06 15:34:50 -0700

    Added basic comments

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,17 @@ use std::io::BufRead;
 use std::io::Write;
 use std::path::Path;
 
+// Basic io Utils used
 struct Utils;
 impl Utils {
+    // Open Rust files, and needed QML Files
     fn open_files(path: &str) -> File {
         match File::open(path) {
             Ok(file) => file,
             Err(_) => panic!("Unable to open file"),
         }
     }
-
+    // Reads the lines of the file and puts it in a Vec<String>
     fn read_file(file: File) -> Vec<String> {
         let reader = BufReader::new(file);
         let lines: Vec<_> = reader.lines().collect();
@@ -31,11 +33,13 @@ impl Utils {
     }
 }
 
+// Main struct for all input files (Rust or QML)
 struct InputFile {
     path: String,
     contents: Vec<String>,
 }
 impl InputFile {
+    // Initiates new file to be opened and read from
     fn new(p: &str) -> InputFile {
         let file = Utils::open_files(p);
         InputFile {
@@ -45,12 +49,15 @@ impl InputFile {
     }
 }
 
+
+// Struct for the Rust input files
 struct RustInFiles {
     file_data: InputFile,
     change_line: String,
     line_number: i32,
 }
 impl RustInFiles {
+    // Initiates rust input files
     fn new(path: &str) -> RustInFiles {
         let file_data = InputFile::new(path);
         let (index, line) = RustInFiles::search(file_data.contents.clone());
@@ -61,6 +68,8 @@ impl RustInFiles {
         }
     }
 
+    // Searches rust files for `load_local_file` and sends out the index in the vec and the whole
+    // line
     fn search(lines: Vec<String>) -> (i32, String) {
         let mut line: String = String::new();
         let mut index: i32 = 0;
@@ -74,6 +83,7 @@ impl RustInFiles {
         (index, line)
     }
 
+    // Splits the searched dir to get the path to the QML file
     fn split_line(&self) -> String {
         let split = self.change_line.as_str().split("\"");
         let vec = split.collect::<Vec<&str>>();
@@ -82,15 +92,19 @@ impl RustInFiles {
     }
 }
 
+// Struct for all QML files
 struct QmlInFile {
     file_data: InputFile,
 }
 impl QmlInFile {
+    // Initializes the QML file
     fn new(path: &str) -> QmlInFile {
         QmlInFile { file_data: InputFile::new(path) }
     }
 }
 
+// Struct for the output file
+// TODO: Not actually output file, but compile from buffer if possible
 struct RustOutFile {
     rust_files: RustInFiles,
     qml_files: QmlInFile,
@@ -105,6 +119,7 @@ impl RustOutFile {
         }
     }
 
+    // Inputs QML String just above `load_local_file` then changes that line to load_data
     fn add_qml(&mut self) {
         let mut new_rust: Vec<String> = Vec::new();
         for (i, l) in self.rust_files.file_data.contents.iter().enumerate() {
@@ -137,6 +152,8 @@ impl RustOutFile {
         self.rust_files.file_data.contents = new_rust;
     }
 
+    // write output file to disk
+    // TODO: Maybe not needed
     fn write_rustfile(&mut self) {
         let rust_file: String = self.rust_files.file_data.contents.iter().cloned().collect();
         let path = Path::new(self.path.as_str());
@@ -154,6 +171,7 @@ impl RustOutFile {
     }
 }
 fn main() {
+    // TODO: get files either automagically or via cli opts.
     let rust_files = RustInFiles::new("markdown.rs");
     let qml_file = QmlInFile::new(rust_files.split_line().as_str());
     let mut out_file = RustOutFile::new(rust_files, qml_file, String::from("out_file.rs"));
